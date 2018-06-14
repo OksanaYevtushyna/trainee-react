98eda306bc5d72574ed2ec80e6079c6bbaf8f1e4
Vlad <gribennikov.vlad@gmail.com>
2018-06-14 18:35:46 +0300

    Add button

--- src/components/AppContainer/index.js
+++ src/components/AppContainer/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from "../ui/Button";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Button />
       </div>
     );
   }
---
+++ src/components/ui/Button.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+import "./Button.css";
+
+const Button = (props) => {
+    return <button className="button">Button</button>
+}
+
+export default Button;
